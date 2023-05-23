890320b6061c40733337f574639d322cd41d25cf
Wouter Jehee <wouterjehee@tutanota.com>
2023-05-23 21:39:06 +0200

    add database adding and retrieving

--- loodsenboekje/__init__.py
+++ loodsenboekje/__init__.py
@@ -1,12 +1,24 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 
 from .rest import Entry, EntryList
+from .db import init_app
 
 
 def create_app():
-    app = Flask(__name__, static_folder='static')
+    app = Flask(__name__, static_folder='static', instance_relative_config=True)
+    app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'loodsenboekje.sqlite'),
+    )
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
     api = Api(app)
+    init_app(app)
 
     @app.route('/')
     def index():
--- loodsenboekje/rest.py
+++ loodsenboekje/rest.py
@@ -1,15 +1,12 @@
-from flask_restful import Resource
 from datetime import datetime
+from flask_restful import Resource, reqparse
+
+from loodsenboekje.db import get_db
 
 
-class EntryItem():
-    def __init__(self, entry_id, description, collaborators, location, timestamp):
-        self.entry_id = entry_id
-        self.description = description
-        self.collaborators = collaborators
-        self.location = location
-        self.timestamp = timestamp
-        # self.timestamp = datetime.now()
+parser = reqparse.RequestParser()
+parser.add_argument('how', type=str, required=True)
+parser.add_argument('who', required=True, action='append')
 
 
 class Entry(Resource):
@@ -27,23 +24,49 @@ class Entry(Resource):
 
 class EntryList(Resource):
     def get(self):
-        return [
-                {
-                    'entry_id': 0,
-                    'description': 'Bier opener',
-                    'collaborators': ['Opa dorus', 'Oma dorus'],
-                    'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                }, {
-                    'entry_id': 1,
-                    'description': '360 bottle flip',
-                    'collaborators': ['Opa dorus'],
-                    'timestamp': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                }
-
-            ]
+        db = get_db()
+        entries = db.execute(
+            """
+            SELECT entry.id, entry.how, GROUP_CONCAT(user.name) as who, entry.created FROM user
+            JOIN entry_user ON user.id = entry_user.user_id
+            JOIN entry ON entry.id = entry_user.entry_id
+            GROUP BY entry.id
+            """
+        ).fetchall()
+        return [{
+            'id': entry["id"],
+            'how': entry["how"],
+            'who': entry["who"].split(','),
+            'timestamp': entry["created"].strftime("%d/%m/%Y, %H:%M:%S")
+        } for entry in entries]
 
     def post(self):
-        # get latest id, increment by one
-        # parse fields
-        # insert and return the obj
-        return {}, 201
+        args = parser.parse_args()
+        db = get_db()
+        cur = db.cursor()
+        # Insert new entry
+        cur.execute(
+            "INSERT INTO entry (how) VALUES (?)",
+            (args["how"],)
+        )
+        entry_id = cur.lastrowid
+        # Insert new persons if they are not already in the database
+        person_ids = []
+        for person in args["who"]:
+            cur.execute(
+                "INSERT OR IGNORE INTO user (name) VALUES (?)",
+                (person,)
+            )
+            person_ids.append(cur.execute(
+                "SELECT * FROM user WHERE name = ?",
+                (person,)
+            ).fetchone()['id'])
+        # Add entry to the join table
+        for person_id in person_ids:
+            cur.execute(
+                "INSERT INTO entry_user (entry_id, user_id) VALUES (?, ?)",
+                (entry_id, person_id)
+            )
+        db.commit()
+
+        return args, 201
