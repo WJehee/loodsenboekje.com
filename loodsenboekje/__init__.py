e816f33a868b443f9c39c290f3a0015f2c081dcb
Wouter Jehee <wouterjehee@tutanota.com>
2023-05-23 11:44:11 +0200

    add some rest stuff and add picocss + update main page

--- loodsenboekje/__init__.py
+++ loodsenboekje/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_restful import Api
+
+from .routes import Entry, bp
 
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app)
 
-    from . import routes
-    app.register_blueprint(routes.bp)
+    app.register_blueprint(bp)
+    api.add_resource(Entry, '/entry/<entry_id>')
 
     return app
--- loodsenboekje/routes.py
+++ loodsenboekje/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_restful import Resource
+from datetime import datetime
 
 
 bp = Blueprint('routes', __name__)
@@ -7,3 +9,33 @@ bp = Blueprint('routes', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+class EntryItem():
+    def __init__(self, entry_id, description, collaborators, location, timestamp):
+        self.entry_id = entry_id
+        self.description = description
+        self.collaborators = collaborators
+        self.location = location
+        self.timestamp = timestamp
+        # self.timestamp = datetime.now()
+
+
+class Entry(Resource):
+    def get(self, entry_id):
+        return {
+            'entry_id': entry_id
+        }
+
+
+class EntryList(Resource):
+    def get(self):
+        # return list of all entries
+        return
+
+    def post(self):
+        # get latest id, increment by one
+        # parse fields
+        # insert and return the obj
+        return {}, 201
+
